ab7c4a28a001648b55ee95baab35140f6b6f0500
Cece Soudaly <cecesoudaly@Ceces-MacBook-Pro.local>
2017-01-13 21:14:59 -0600

    more files

---
+++ Meme2/MemeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MemeCollectionViewCell.swift
+//  Meme2
+//
+//  Created by Cece Soudaly on 1/10/17.
+//  Copyright © 2017 CeceMobile. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var TopLabel: UILabel!
+    @IBOutlet weak var BottomLabel: UILabel!
+    
+}
---
+++ Meme2/SentMemeTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SentMemeTableViewController.swift
+//  Meme2
+//
+//  Created by Cece Soudaly on 1/12/17.
+//  Copyright © 2017 CeceMobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class SentMemeTableViewController: UICollectionViewController {
+}
